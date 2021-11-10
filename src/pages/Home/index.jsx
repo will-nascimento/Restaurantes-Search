951ada853faf00b0215fe95618087488255c4de5
Will <willnascimentowns@gmail.com>
2021-11-10 18:05:19 -0300

    Restaurants-Search

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import logo from "../../assets/logo.svg"
+
+import { Container, Search } from "./styles";
+
+const Home = () => 
+    <Container>
+        <Search>Olá</Search>
+    </Container>;
+
+export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div '
+    background-color: black;
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto;
+';
+
+export const Search = styled.section `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #ffffff;
+`;
